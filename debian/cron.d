d065347869251d626d65f8f7f7f0499a5df51b65
MD <4d876b82@gmail.com>
2009-09-25 23:05:17 +0800

    debian/cron.d: удаление кэшированных файлов старше суток

--- debian/cron.d
+++ debian/cron.d
@@ -1,4 +1,5 @@
 #
 # Regular cron jobs for the librusec package
 #
-#0 4	* * *	root	[ -x /usr/bin/librusec_maintenance ] && /usr/bin/librusec_maintenance
+0 4	* * *	www-data	find /usr/share/drupal6/cache/ -type f -mtime +1 -delete
+
